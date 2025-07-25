c921b598e2f3af2e5cb665027fd7d2a25f1dde14
zhangsong <s.zhang.cedar@gmail.com>
2025-07-25 17:41:02 +0800

    del

--- test/test.py
+++
@@ -1,9 +0,0 @@
-import pkg_resources
-
-args = []
-for dist in pkg_resources.working_set:
-    # 过滤掉部分系统包或你不想打包的包
-    if dist.project_name.lower() not in ["pip", "setuptools", "wheel"]:
-        args.append(f"--include-package={dist.project_name}")
-
-print(" ".join(args))
